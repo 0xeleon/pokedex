3752927dec09ce6af939a929a79cf5f1264c2a4a
Eduardo Leon <eduardo@weareatomic.com>
2022-08-08 14:47:10 -0500

    add interceptor http and update test conf

--- src/app/interfaces/ pokemonApi.model.ts
+++ src/app/interfaces/ pokemonApi.model.ts
@@ -14,3 +14,11 @@ export interface testInterface {
     name : string,
     url : string
 }
+
+export interface detailPokemonInterface {
+    name : string,
+    weight : number,
+    image : string
+    abilities : string[],
+    load : boolean
+}
