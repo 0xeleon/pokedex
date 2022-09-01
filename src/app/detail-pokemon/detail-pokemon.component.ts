05c91a837f8dd510645c0cc997daef78baeb4c0e
Eduardo Leon <eduardo@weareatomic.com>
2022-08-31 20:02:31 -0500

    share pokemonid with observables

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Router,ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Subject, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { PokemonService } from '../services/pokemon.service';
+import { PaginationService, Page } from '../services/pagination.service'
 import { detailPokemonInterface } from '../interfaces/ pokemonApi.model';
 
 @Component({
@@ -9,16 +13,19 @@ import { detailPokemonInterface } from '../interfaces/ pokemonApi.model';
   templateUrl: './detail-pokemon.component.html',
   styleUrls: ['./detail-pokemon.component.css']
 })
-export class DetailPokemonComponent implements OnInit {
+
+export class DetailPokemonComponent implements OnInit, OnDestroy {
   private sub: any;
   private pokemonId : any;
   private urlImage : string;
   public pokemonDetail : detailPokemonInterface;
+  public numberPokemon$ : Subscription;
+  public onDestroy$ : Subject<boolean> = new Subject()
 
+  constructor(private router:Router,
+              private pokemonService: PokemonService,
+              paginationService : PaginationService) { 
 
-  constructor(private activatedRoute:ActivatedRoute,
-              private router:Router,
-              private pokemonService: PokemonService) { 
     this.urlImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`;
     this.pokemonDetail = {
       name : '',
@@ -27,6 +34,19 @@ export class DetailPokemonComponent implements OnInit {
       abilities : [],
       load : false
     }
+    
+    /** Example variable without subscribe IMPORTANT the variable must be  Observable<PokemonSelected>;
+     * this.numberPokemon$ = pokemonService.sharingObservablePokemonSelected;
+    */
+
+    this.numberPokemon$ = pokemonService.sharingObservablePokemonSelected
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe(
+      (data) => {
+        console.log('number pokemon', data)
+        this.pokemonId = data.pokemonId;
+      }
+    );
   }
 
   public goToList() : void {
@@ -34,23 +54,24 @@ export class DetailPokemonComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // detect params from url
-    this.sub = this.activatedRoute.queryParams.subscribe(params => {
-      this.pokemonId = params['pokemonId'];
-      if(this.pokemonId){
-        // Call to get the details of pokemon api
-        this.pokemonService.getPokemonDetails(this.pokemonId).subscribe(data => {
-          if(data){
-            this.pokemonDetail.name = data.name;
-            this.pokemonDetail.weight = data.weight;
-            this.pokemonDetail.image = `${this.urlImage}${this.pokemonId}.png`;
-            this.pokemonDetail.abilities = data.abilities.map((item:any) => item.ability.name);
-            this.pokemonDetail.load = true;
+    if(this.pokemonId){
+      this.pokemonService.getPokemonDetails(this.pokemonId).subscribe(data => {
+        if(data){
+          this.pokemonDetail = {
+            name : data.name,
+            weight : data.weight,
+            image : `${this.urlImage}${this.pokemonId}.png`,
+            abilities : data.abilities.map((item:any) => item.ability.name),
+            load : true 
           }
-        })
-      }else{
-        this.router.navigate(['/list-pokemon']);
-      }
-    });
+        }
+      });
+    }else {
+      this.router.navigate(['/list-pokemon']);
+    }
+  }
+  
+  ngOnDestroy(){
+    this.onDestroy$.next(true);
   }
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,21 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+export interface PokemonSelected {
+  pokemonId : number | null
+}
 @Injectable({
   providedIn: 'root'
 })
+
 export class PokemonService {
   private url = 'https://pokeapi.co/api/v2/';
-  
+  private sharingObservablePokemon$ : BehaviorSubject<PokemonSelected> = new BehaviorSubject<PokemonSelected>({pokemonId : null});
+
   constructor(private httpClient: HttpClient) { }
 
   getPokemonList(limit : number, offset : number) :Observable<any>{
-    return this.httpClient.get(`${this.url}pokemon?limit=${limit}&offset=${offset}`);
+    return this.httpClient.get(`${this.url}pokemon?limit=${limit}&offset=${offset}`)
+      .pipe(map(res => {
+        return res
+      }))
   }
 
   getPokemonDetails(idPokemon : number) : Observable<any>{
     return this.httpClient.get(`${this.url}pokemon/${idPokemon}/`);
   }
 
+  get sharingObservablePokemonSelected(){
+    return this.sharingObservablePokemon$.asObservable();
+  }
+
+  set sharingObservablePokemonSelectedData(data : PokemonSelected){
+    this.sharingObservablePokemon$.next(data);
+  }
 }
